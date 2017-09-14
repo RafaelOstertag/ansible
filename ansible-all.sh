c95a471b398c0ce680d3d65a3ea11d18bbc2c6b3
Rafael Ostertag <rafi@guengel.ch>
2017-09-14 20:20:10 +0000

    Added script to run all playbooks.

---
+++ ansible-all.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+trap { rm -f vaultpass } EXIT
+
+echo "Enter vault password"
+stty -echo
+read password
+stty echo
+
+echo "$password" > vaultpass
+unset password
+
+for f in freebsd-servers.yml freebsd-workstations.yml openbsd-servers.yml openbsd-virtual-hosts. solaris11-servers.yml solaris11-workstations.yml
+do
+    ansible -i hosts --vault-password-file=vaultpass "$f"
+done

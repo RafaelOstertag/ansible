fc776b68441b3931035f677ca57a23f3cb0f5a1b
Rafael Ostertag <rafi@guengel.ch>
2017-10-01 12:11:51 +0000

    Removed options now in ansible.cfg.

--- ansible-all.sh
+++ ansible-all.sh
@@ -1,18 +1,8 @@
 #!/bin/sh
 
-trap "rm -f vaultpass" EXIT
-
-echo "Enter vault password"
-stty -echo
-read password
-stty echo
-
-echo "$password" > vaultpass
-unset password
-
 echo "Commencing run"
 
 for f in freebsd-servers.yml freebsd-workstations.yml openbsd-servers.yml openbsd-virtual-hosts.yml
 do
-    ansible-playbook -f 20 -i hosts --vault-password-file=vaultpass "$f"
+    ansible-playbook -f 20 "$f"
 done

eb34af17037e12ac1b4d8919fad92e8f58a77d55
rafi@guengel.ch <rafi@guengel.ch>
2019-07-31 12:22:27 +0200

    Several playbooks can be specified

--- ansible-all.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-echo "Commencing run"
-
-for f in freebsd-servers.yml freebsd-workstations.yml openbsd-servers.yml openbsd-virtual-hosts.yml freebsd-webserver.yml
-do
-    ansible-playbook -f 20 "$f"
-done

7c2d16194f9cad3c2193fccbcd8863c41ac9aa08
rafi@guengel.ch <rafi@guengel.ch>
2019-08-10 23:29:00 +0200

    Support custom vhosts on ec2

--- roles/ec2/web/nginx/templates/certbot.sh
+++ roles/ec2/web/nginx/templates/certbot.sh
@@ -4,4 +4,5 @@
 #
 /usr/local/bin/certbot \
     certonly --webroot -w /data/www/nginx-letsencrypt \
-    -d {{ nginx_domains |join(' -d ') }} 
+    -d {{ nginx_domains | join(' -d ') }} \
+    -d {{ nginx_custom_domains | join(' -d ') }}

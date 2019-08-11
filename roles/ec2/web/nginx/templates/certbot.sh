4d22e52140878b18f2f67fb491e3b5bf04b4fa25
rafi@guengel.ch <rafi@guengel.ch>
2019-08-11 10:40:31 +0200

    Restart nginx after updating certificates

--- roles/ec2/web/nginx/templates/certbot.sh
+++ roles/ec2/web/nginx/templates/certbot.sh
@@ -2,7 +2,11 @@
 #
 # Create letsencrypt certificates
 #
+
+set -e
+
 /usr/local/bin/certbot \
     certonly --webroot -w /data/www/nginx-letsencrypt \
     -d {{ nginx_domains | join(' -d ') }} \
     -d {{ nginx_custom_domains | join(' -d ') }}
+service nginx restart

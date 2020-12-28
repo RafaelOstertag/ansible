bc6efe9fa03380cd16465cc6d5dbc3e2abe46066
Rafael Ostertag <rafi@guengel.ch>
2020-12-28 21:27:13 +0100

    Fix node-exporter-zpool.sh

--- roles/freebsd/node-exporter/files/node-exporter-zpool.sh
+++ roles/freebsd/node-exporter/files/node-exporter-zpool.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+set -eu
+
 ZPOOL=/sbin/zpool
 if [ ! -f "${ZPOOL}" ]
 then
@@ -7,22 +9,24 @@ then
     exit 1
 fi
 
-${ZPOOL} list -Hp | while read name size alloc free ckpoint expandsz frag cap dedup health rest
+SIZE=""
+ALLOCATED=""
+FREE=""
+HEALTHY=""
+OLDIFS="${IFS}"
+IFS=$'
+'
+for l in `${ZPOOL} list -Hp`
 do
-    echo "# HELP zpool_size size in bytes of zpool"
-    echo "# TYPE zpool_size gauge"
-    echo "zpool_size{name=\"$name\"} $size"
+    name=`echo $l | awk '{ print $1 }'`
+    size=`echo $l | awk '{ print $2 }'`
+    alloc=`echo $l | awk '{ print $3 }'`
+    free=`echo $l | awk '{ print $4 }'`
+    health=`echo $l | awk '{ print $10 }'`
+    SIZE="${SIZE}zpool_size{name=\"$name\"} $size\n"
+    ALLOCATED="${ALLOCATED}zpool_allocated{name=\"$name\"} $alloc\n"
+    FREE="${FREE}zpool_free{name=\"$name\"} $free\n"
 
-    echo "# HELP zpool_allocated allocated bytes of zpool"
-    echo "# TYPE zpool_allocated gauge"
-    echo "zpool_allocated{name=\"$name\"} $alloc"
-
-    echo "# HELP zpool_free free bytes of zpool"
-    echo "# TYPE zpool_free gauge"
-    echo "zpool_free{name=\"$name\"} $free"
-
-    echo "# HELP zpool_healthy flag indicating if zpool is health"
-    echo "# TYPE zpool_healthy gauge"
     if [ "${health}" == "ONLINE" ]
     then
         healthy=1
@@ -30,5 +34,31 @@ do
         healthy=0
     fi
 
-    echo "zpool_healthy{name=\"$name\"} $healthy"
+    HEALTHY="${HEALTHY}zpool_healthy{name=\"$name\"} $healthy\n"
 done
+IFS="${OLDIFS}"
+
+if [ -n "${SIZE}" ]
+then
+    echo "# HELP zpool_size size in bytes of zpool"
+    echo "# TYPE zpool_size gauge"
+    echo -e "$SIZE" | sed '/^$/d'
+fi
+if [ -n "${ALLOCATED}" ]
+then
+    echo "# HELP zpool_allocated allocated bytes of zpool"
+    echo "# TYPE zpool_allocated gauge"
+    echo -e "$ALLOCATED" | sed '/^$/d'
+fi
+if [ -n "${FREE}" ]
+then
+    echo "# HELP zpool_free free bytes of zpool"
+    echo "# TYPE zpool_free gauge"
+    echo -e "$FREE" | sed '/^$/d'
+fi
+if [ -n "${HEALTHY}" ]
+then
+    echo "# HELP zpool_healthy flag indicating if zpool is health"
+    echo "# TYPE zpool_healthy gauge"
+    echo -e "$HEALTHY" | sed '/^$/d'
+fi

64b766fb5a73f7f83b871762e525c015cb6c0105
Rafael Ostertag <rafi@guengel.ch>
2024-10-31 21:55:14 +0100

    tweak borg-backup.sh

--- roles/borg-client/templates/borg-backup.sh
+++ roles/borg-client/templates/borg-backup.sh
@@ -25,16 +25,13 @@ init_repo() {
 }
 
 initialize_repo_if_required() {
-    if ! check_repo
-    then
-        if ! init_repo
-        then
+    if ! check_repo; then
+        if ! init_repo; then
             log_error "cannot initialize repo"
             exit 1
         fi
 
-        if ! check_repo
-        then
+        if ! check_repo; then
             log_error "repo initialized but cannot be checked"
             exit 2
         fi
@@ -45,37 +42,37 @@ initialize_repo_if_required() {
 
 initialize_repo_if_required
 
-borg create  --verbose                       \
+borg create --verbose \
     --list \
     --filter CE \
-    --stats                         \
-    --compression lz4               \
+    --stats \
+    --compression lz4 \
+{% if 'ec2_borg_clients' in group_names %}
+    --upload-ratelimit 5000 \
+{% endif %}
     --exclude-from /usr/local/etc/borg-exclude \
-    ::'{now}'            \
+    ::'{now}' \
     /
 errval=$?
-if [ ${errval} -ne 0 -a \! \( ${errval} -ge 100 -a ${errval} -le 127 \) ]
-then
+if [ ${errval} -ne 0 -a \! \( ${errval} -ge 100 -a ${errval} -le 127 \) ]; then
     log_error "Error while backing up. Error code: ${errval}"
     exit 4
 fi
 
 borg prune --verbose \
- --stats \
- --keep-daily 7 \
-  --keep-weekly 4 \
-  --keep-monthly 12
+    --stats \
+    --keep-daily 7 \
+    --keep-weekly 4 \
+    --keep-monthly 12
 errval=$?
-if [ ${errval} -ne 0 ]
-then
+if [ ${errval} -ne 0 ]; then
     log_error "Error while pruning. Error code: ${errval}"
     exit 5
 fi
 
 borg compact
 errval=$?
-if [ ${errval} -ne 0 ]
-then
+if [ ${errval} -ne 0 ]; then
     log_error "Error while compacting. Error code: ${errval}"
     exit 5
 fi
